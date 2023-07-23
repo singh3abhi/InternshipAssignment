795e901b7bc526efc66095893bb301fa9772a7c6
singh3abhi <singh3abhi@gmail.com>
2023-07-23 14:49:31 +0530

    Added Global Constants

---
+++ lib/constants/global_variables.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class GlobalVariables {
+  // Colors
+  static const primaryColor = Color.fromARGB(255, 44, 46, 67);
+  static const backgroundColor = Colors.white;
+
+  static const List<String> brandCategoryImages = [
+    'assets/Categories/apple.png',
+    'assets/Categories/samsung.png',
+    'assets/Categories/mi.png',
+    'assets/Categories/vivo.png',
+    'assets/Categories/oneplus.png',
+    'assets/Categories/oppo.png',
+    'assets/Categories/motorola.png',
+    'assets/Categories/realme.png',
+  ];
+
+  static const List<ShopByCategoryItem> shopByCategoryList = [
+    ShopByCategoryItem(name: 'Bestselling Mobiles', imageURL: 'assets/ShopBy/1.png'),
+    ShopByCategoryItem(name: 'Verified Devices Only', imageURL: 'assets/ShopBy/2.png'),
+    ShopByCategoryItem(name: 'Like New Condition', imageURL: 'assets/ShopBy/3.png'),
+    ShopByCategoryItem(name: 'Phones with Warranty', imageURL: 'assets/ShopBy/4.png'),
+    ShopByCategoryItem(name: 'Shop By Price', imageURL: 'assets/ShopBy/5.png'),
+  ];
+}
+
+class ShopByCategoryItem {
+  final String name;
+  final String imageURL;
+
+  const ShopByCategoryItem({
+    required this.name,
+    required this.imageURL,
+  });
+}
