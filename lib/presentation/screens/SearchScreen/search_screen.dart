202296f4c4dc30fd21a1d1252c1eb6e8b87ce7f9
singh3abhi <singh3abhi@gmail.com>
2023-07-28 20:00:46 +0530

    Few updates

--- lib/presentation/screens/SearchScreen/search_screen.dart
+++ lib/presentation/screens/SearchScreen/search_screen.dart
@@ -24,96 +24,94 @@ class _SearchScreenState extends State<SearchScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: GlobalVariables.backgroundColor,
-        key: scaffoldKey,
-        drawer: const Drawer(
-          child: Text('create drawer widget tree here'),
-        ),
-        appBar: CustomAppBar(scaffoldKey: scaffoldKey),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: GlobalVariables.primaryColor,
-          shape: const CircleBorder(),
-          child: const Center(
-            child: Icon(
-              Icons.arrow_circle_up,
-              size: 40,
-              color: Colors.white,
-            ),
+    return Scaffold(
+      backgroundColor: GlobalVariables.backgroundColor,
+      key: scaffoldKey,
+      drawer: const Drawer(
+        child: Text('create drawer widget tree here'),
+      ),
+      appBar: CustomAppBar(scaffoldKey: scaffoldKey),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: GlobalVariables.primaryColor,
+        shape: const CircleBorder(),
+        child: const Center(
+          child: Icon(
+            Icons.arrow_circle_up,
+            size: 40,
+            color: Colors.white,
           ),
         ),
-        body: BlocBuilder<SearchModelCubit, SearchModelState>(
-          builder: (context, state) {
-            if (state is SearchModelInitialState) {
-              return const SizedBox();
-            }
+      ),
+      body: BlocBuilder<SearchModelCubit, SearchModelState>(
+        builder: (context, state) {
+          if (state is SearchModelInitialState) {
+            return const SizedBox();
+          }
 
-            if (state is SearchModelLoadingState) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+          if (state is SearchModelLoadingState) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
 
-            if (state is SearchModelErrorState) {
-              return const Center(
-                child: Text('Some error has occured'),
-              );
-            }
+          if (state is SearchModelErrorState) {
+            return const Center(
+              child: Text('Some error has occured'),
+            );
+          }
 
-            if (state is SearchModelLoadedState && state.makes.isEmpty && state.models.isEmpty) {
-              return const Padding(
-                padding: EdgeInsets.only(top: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('No Results Found'),
-                  ],
-                ),
-              );
-            }
-            return SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Brand',
-                      style: TextStyle(fontSize: 10.5, color: Colors.black54),
-                    ),
-                    const SizedBox(height: 10),
-                    ListView.separated(
-                      itemCount: state.makes.length,
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      separatorBuilder: (context, index) => const SizedBox(height: 20),
-                      itemBuilder: (context, index) {
-                        return Text(state.makes[index]);
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      'Mobile Model',
-                      style: TextStyle(fontSize: 10.5, color: Colors.black54),
-                    ),
-                    const SizedBox(height: 10),
-                    ListView.separated(
-                      itemCount: state.models.length,
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      separatorBuilder: (context, index) => const SizedBox(height: 20),
-                      itemBuilder: (context, index) {
-                        return Text(state.models[index]);
-                      },
-                    ),
-                  ],
-                ),
+          if (state is SearchModelLoadedState && state.makes.isEmpty && state.models.isEmpty) {
+            return const Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('No Results Found'),
+                ],
               ),
             );
-          },
-        ),
+          }
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Brand',
+                    style: TextStyle(fontSize: 10.5, color: Colors.black54),
+                  ),
+                  const SizedBox(height: 10),
+                  ListView.separated(
+                    itemCount: state.makes.length,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    separatorBuilder: (context, index) => const SizedBox(height: 20),
+                    itemBuilder: (context, index) {
+                      return Text(state.makes[index]);
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Mobile Model',
+                    style: TextStyle(fontSize: 10.5, color: Colors.black54),
+                  ),
+                  const SizedBox(height: 10),
+                  ListView.separated(
+                    itemCount: state.models.length,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    separatorBuilder: (context, index) => const SizedBox(height: 20),
+                    itemBuilder: (context, index) {
+                      return Text(state.models[index]);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
